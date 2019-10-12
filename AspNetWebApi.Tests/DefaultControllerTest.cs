d5572c3f1cc7ee031fbdb8e07f79e2fa84fe3797
sstk14 <mail>
2019-10-12 12:56:57 +0900

    ASP.NET WebApi  + 単体テスト サンプル

---
+++ AspNetWebApi.Tests/DefaultControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace AspNetWebApi.Tests
+{
+    [TestClass]
+    public class DefaultControllerTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testProducts = GetTestProducts();
+            var controller = new AspNetWebApi.Controller.DefaultController();
+            var result = controller.Get();
+        }
+    }
+}
---
+++ AspNetWebApi/Controller/DefaultController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetWebApi.Controller
+{
+    public class DefaultController : ApiController
+    {
+        public DefaultController() { }
+
+        // GET: api/Default1
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default1/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Default1
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Default1/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Default1/5
+        public void Delete(int id)
+        {
+        }
+
+        List<Models.Product> products = new List<Models.Product>();
+        public IEnumerable<Models.Product> GetAllProducts()
+        {
+            return products;
+        }
+    }
+}
